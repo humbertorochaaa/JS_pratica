28b6c5c9d54ea2afac263a0ae3fbc424e37687ed
humbertorochaaa <humbertorochaaa@users.noreply.github.com>
2020-09-23 17:29:43 -0300

    Atualização do meu primeiro projeto

---
+++ ContaCorrente.js
@@ -0,0 +1,23 @@
+class ContaCorrente{
+    agen;
+    saldo;
+
+    sacar(Valor){
+        if (this.saldo >= Valor) {
+            this.saldo += Valor;
+            
+        }
+    }
+  
+    depositar(Valor){
+        if (Valor >0) {
+            this.saldo += Valor;
+            
+        }
+    }
+       
+           
+       
+
+    
+}
--- Index.js
+++ Index.js
@@ -1,31 +1,6 @@
-class Cliente {
-    nome;
-    cpf;
-}
 
-class ContaCorrente{
-    agen;
-    saldo;
 
-    sacar(Valor){
-        if (this.saldo >= Valor) {
-            this.saldo += Valor;
-            
-        }
-    }
-  
-    depositar(Valor){
-        if (Valor >0) {
-            this.saldo += Valor;
-            
-        }
-    }
-       
-           
-       
 
-    
-}
 
 
 
